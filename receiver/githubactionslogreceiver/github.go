f12365fc67a701be11f82f4ba919da3abf1dcbb7
Jan Calanog <jan.calanog@elastic.co>
2024-04-16 21:41:25 +0200

    refactor

---
+++ receiver/githubactionslogreceiver/github.go
@@ -0,0 +1,159 @@
+package githubactionslogreceiver
+
+import (
+	"archive/zip"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v60/github"
+	"go.uber.org/zap"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func createGitHubClient(githubAuth GitHubAuth) (*github.Client, error) {
+	if githubAuth.AppID != 0 {
+		if githubAuth.PrivateKey != "" {
+			var privateKey []byte
+			privateKey, err := base64.StdEncoding.DecodeString(string(githubAuth.PrivateKey))
+			if err != nil {
+				privateKey = []byte(githubAuth.PrivateKey)
+			}
+			itr, err := ghinstallation.New(
+				http.DefaultTransport,
+				githubAuth.AppID,
+				githubAuth.InstallationID,
+				privateKey,
+			)
+			if err != nil {
+				return &github.Client{}, err
+			}
+			return github.NewClient(&http.Client{Transport: itr}), nil
+		} else {
+			itr, err := ghinstallation.NewKeyFromFile(
+				http.DefaultTransport,
+				githubAuth.AppID,
+				githubAuth.InstallationID,
+				githubAuth.PrivateKeyPath,
+			)
+			if err != nil {
+				return &github.Client{}, err
+			}
+			return github.NewClient(&http.Client{Transport: itr}), nil
+		}
+	} else {
+		return github.NewClient(nil).WithAuthToken(string(githubAuth.Token)), nil
+	}
+}
+
+func fetchLog(httpClient *http.Client, logURL string) (io.ReadCloser, error) {
+	req, err := http.NewRequest("GET", logURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("failed to get logs: %s", resp.Status)
+	}
+	return resp.Body, nil
+}
+
+type DeleteFunc func() error
+
+func getRunLog(
+	cache runLogCache,
+	logger *zap.Logger,
+	ctx context.Context,
+	ghClient *github.Client,
+	httpClient *http.Client,
+	repository *github.Repository,
+	workflowRun *github.WorkflowRun,
+) (*zip.ReadCloser, DeleteFunc, error) {
+	filename := fmt.Sprintf("run-log-%d-%d.zip", workflowRun.ID, workflowRun.GetRunStartedAt().Unix())
+	fp := filepath.Join(os.TempDir(), "run-log-cache", filename)
+	if !cache.Exists(fp) {
+		logURL, _, err := ghClient.Actions.GetWorkflowRunLogs(
+			ctx,
+			repository.GetOwner().GetLogin(),
+			repository.GetName(),
+			workflowRun.GetID(),
+			4,
+		)
+		if err != nil {
+			logger.Error("Failed to get logs download url", zap.Error(err))
+			return nil, nil, err
+		}
+		response, err := fetchLog(httpClient, logURL.String())
+		defer response.Close()
+		if err != nil {
+			return nil, nil, err
+		}
+		err = cache.Create(fp, response)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	zipFile, err := cache.Open(fp)
+	deleteFunc := func() error {
+		return os.Remove(fp)
+	}
+	return zipFile, deleteFunc, err
+}
+
+// This function takes a zip file of logs and a list of Jobs.
+// Structure of zip file
+//
+//	zip/
+//	├── jobname1/
+//	│   ├── 1_stepname.txt
+//	│   ├── 2_anotherstepname.txt
+//	│   ├── 3_stepstepname.txt
+//	│   └── 4_laststepname.txt
+//	└── jobname2/
+//	    ├── 1_stepname.txt
+//	    └── 2_somestepname.txt
+//
+// It iterates through the list of Jobs and tries to find the matching
+// log in the zip file. If the matching log is found it is attached
+// to the job.
+func attachRunLog(rlz *zip.Reader, jobs []Job) {
+	for i, job := range jobs {
+		for j, step := range job.Steps {
+			re := logFilenameRegexp(job, step)
+			for _, file := range rlz.File {
+				fileName := file.Name
+				if re.MatchString(fileName) {
+					jobs[i].Steps[j].Log = file
+					break
+				}
+			}
+		}
+	}
+}
+
+// Copied from https://github.com/cli/cli/blob/b54f7a3bde50df3c31fdd68b638a0c0378a0ad58/pkg/cmd/run/view/view.go#L493
+// to handle the case where the job or step name in the zip file is different from the job or step name in the object
+func logFilenameRegexp(job Job, step Step) *regexp.Regexp {
+	// As described in https://github.com/cli/cli/issues/5011#issuecomment-1570713070, there are a number of steps
+	// the server can take when producing the downloaded zip file that can result in a mismatch between the job name
+	// and the filename in the zip including:
+	//  * Removing characters in the job name that aren't allowed in file paths
+	//  * Truncating names that are too long for zip files
+	//  * Adding collision deduplicating numbers for Jobs with the same name
+	//
+	// We are hesitant to duplicate all the server logic due to the fragility but while we explore our options, it
+	// is sensible to fix the issue that is unavoidable for users, that when a job uses a composite action, the server
+	// constructs a job name by constructing a job name of `<JOB_NAME`> / <ACTION_NAME>`. This means that logs will
+	// never be found for Jobs that use composite actions.
+	sanitizedJobName := strings.ReplaceAll(job.Name, "/", "")
+	re := fmt.Sprintf(`%s\/%d_.*\.txt`, regexp.QuoteMeta(sanitizedJobName), step.Number)
+	return regexp.MustCompile(re)
+}
--- receiver/githubactionslogreceiver/receiver_test.go
+++ receiver/githubactionslogreceiver/receiver_test.go
@@ -4,15 +4,10 @@ import (
 	"archive/zip"
 	"bytes"
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
 	"github.com/google/go-github/v60/github"
 	"github.com/h2non/gock"
 	"github.com/stretchr/testify/assert"
-	"go.opentelemetry.io/collector/config/configopaque"
 	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/plog"
@@ -27,81 +22,6 @@ import (
 	"testing"
 	"time"
 )
-
-func TestAttachRunLog(t *testing.T) {
-	// arrange
-	buf := new(bytes.Buffer)
-	func() {
-		writer := zip.NewWriter(buf)
-		defer writer.Close()
-		file, err := writer.Create(filepath.Join("SomeJob", "1_stepname.txt"))
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = file.Write([]byte(""))
-		if err != nil {
-			t.Fatal(err)
-		}
-	}()
-	reader := bytes.NewReader(buf.Bytes())
-	zipReader, err := zip.NewReader(reader, int64(len(buf.Bytes())))
-	if err != nil {
-		t.Fatal(err)
-	}
-	jobs := []Job{
-		{
-			Name: "SomeJob",
-			Steps: Steps{
-				{
-					Number: 1,
-				},
-			},
-		},
-	}
-
-	// act
-	attachRunLog(zipReader, jobs)
-	// assert
-	assert.NotNil(t, jobs[0].Steps[0].Log)
-}
-
-func TestAttachRunLog2(t *testing.T) {
-	// arrange
-	buf := new(bytes.Buffer)
-	func() {
-		writer := zip.NewWriter(buf)
-		defer writer.Close()
-		file, err := writer.Create(filepath.Join("job  action", "1_stepname.txt"))
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = file.Write([]byte(""))
-		if err != nil {
-			t.Fatal(err)
-		}
-	}()
-	reader := bytes.NewReader(buf.Bytes())
-	zipReader, err := zip.NewReader(reader, int64(len(buf.Bytes())))
-	if err != nil {
-		t.Fatal(err)
-	}
-	jobs := []Job{
-		{
-			Name: "job / action",
-			Steps: Steps{
-				{
-					Number: 1,
-				},
-			},
-		},
-	}
-
-	// act
-	attachRunLog(zipReader, jobs)
-
-	// assert
-	assert.NotNil(t, jobs[0].Steps[0].Log)
-}
 
 func TestHealthCheckHandler(t *testing.T) {
 	ghalr := githubActionsLogReceiver{}
@@ -245,96 +165,6 @@ func TestWorkflowRunHandlerRequestedAction(t *testing.T) {
 
 	// assert
 	assert.Equal(t, http.StatusOK, w.Code)
-}
-
-func TestCreateGitHubClient(t *testing.T) {
-	// arrange
-	ghAuth := GitHubAuth{
-		Token: "token",
-	}
-
-	// act
-	_, err := createGitHubClient(ghAuth)
-
-	// assert
-	assert.NoError(t, err)
-}
-
-func TestCreateGitHubClient2(t *testing.T) {
-	// arrange private key
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return
-	}
-	pkcs1PrivateKey := x509.MarshalPKCS1PrivateKey(privateKey)
-	privateKeyBlock := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: pkcs1PrivateKey,
-	}
-	fp := filepath.Join(os.TempDir(), "private.*.pem")
-	file, err := os.Create(fp)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer file.Close()
-	if err := pem.Encode(file, privateKeyBlock); err != nil {
-		t.Fatal(err)
-	}
-
-	// arrange receiver config
-	ghAuth := GitHubAuth{
-		AppID:          123,
-		InstallationID: 123,
-		PrivateKeyPath: fp,
-	}
-
-	// act
-	_, err = createGitHubClient(ghAuth)
-
-	// assert
-	assert.NoError(t, err)
-}
-
-func TestCreateGitHubClient3(t *testing.T) {
-	// arrange private key
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return
-	}
-	pkcs1PrivateKey := x509.MarshalPKCS1PrivateKey(privateKey)
-	privateKeyBlock := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: pkcs1PrivateKey,
-	}
-	encodedPrivateKey := pem.EncodeToMemory(privateKeyBlock)
-
-	// arrange receiver config
-	ghAuth := GitHubAuth{
-		AppID:          123,
-		InstallationID: 123,
-		PrivateKey:     configopaque.String(encodedPrivateKey),
-	}
-
-	// act
-	_, err = createGitHubClient(ghAuth)
-
-	// assert
-	assert.NoError(t, err)
-}
-
-func TestCreateGitHubClient4(t *testing.T) {
-	// arrange
-	ghAuth := GitHubAuth{
-		AppID:          123,
-		InstallationID: 123,
-		PrivateKey:     "malformed private key",
-	}
-
-	// act
-	_, err := createGitHubClient(ghAuth)
-
-	// assert
-	assert.EqualError(t, err, "could not parse private key: invalid key: Key must be a PEM encoded PKCS1 or PKCS8 key")
 }
 
 type failingConsumer struct {
